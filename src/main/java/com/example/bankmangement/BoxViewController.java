5c7ea99612dcfd62b0a62233fc2d6d34cab23156
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-03-30 00:42:52 +0600

    Login Page added

--- src/main/java/com/example/bankmangement/BoxViewController.java
+++ src/main/java/com/example/bankmangement/BoxViewController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,19 +14,24 @@ public class BoxViewController implements Initializable {
     @FXML
     private TableView<Box> table;
     @FXML
-    private TableColumn<Box,Integer> boxIDColumn;
+    private TableColumn<Box, Integer> boxIDColumn;
     @FXML
-    private TableColumn<Box,Integer> heightColumn;
+    private TableColumn<Box, Integer> heightColumn;
     @FXML
-    private TableColumn<Box,Integer> widthColumn;
+    private TableColumn<Box, Integer> widthColumn;
     @FXML
-    private TableColumn<Box,Integer> oldPriceColumn;
+    private TableColumn<Box, Integer> oldPriceColumn;
     @FXML
-    private TableColumn<Box,Integer> newPriceColumn;
+    private TableColumn<Box, Integer> newPriceColumn;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        boxIDColumn.setCellValueFactory(new PropertyValueFactory<Box, Integer>("boxID"));
+        heightColumn.setCellValueFactory(new PropertyValueFactory<Box, Integer>("height"));
+        widthColumn.setCellValueFactory(new PropertyValueFactory<Box, Integer>("width"));
+        oldPriceColumn.setCellValueFactory(new PropertyValueFactory<Box, Integer>("oldPrice"));
+        newPriceColumn.setCellValueFactory(new PropertyValueFactory<Box, Integer>("newPrice"));
 
     }
 }
