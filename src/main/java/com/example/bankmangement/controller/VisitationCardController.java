cceb18b04186eb1730ccea4354411b71b7fec410
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-03-30 18:09:56 +0600

    Login Page added

--- src/main/java/com/example/bankmangement/controller/VisitationCardController.java
+++ src/main/java/com/example/bankmangement/controller/VisitationCardController.java
@@ -1,15 +1,19 @@
 package com.example.bankmangement.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class VisitationCardController implements Initializable {
@@ -27,14 +31,13 @@ public class VisitationCardController implements Initializable {
     private CheckBox formAsDeputyCheckBox;
 
     //description tab
-    @FXML
-    private Text descriptionTabBoxNoText;
-    @FXML
-    private Text descriptionTabProviedBoxNoText;
+
     @FXML
     private ImageView descriptionTabSignature;
     @FXML
     private ImageView descriptionTabTodaySignature;
+    @FXML
+    private ImageView descriptionTabAttendantSignature;
     @FXML
     private Button descriptionTabGrantButton;
     @FXML
@@ -42,6 +45,8 @@ public class VisitationCardController implements Initializable {
 
 
     //Card tab
+    @FXML
+    private Text cardTabBoxNoText;
     @FXML
     private Text cardTabDescriptionText;
     @FXML
@@ -53,12 +58,58 @@ public class VisitationCardController implements Initializable {
     @FXML
     private ImageView cardTabSignature;
     @FXML
-    private ImageView cardTabAtteandeSignature;
+    private ImageView cardTabAttendantSignature;
     @FXML
     private Button cardTabDoneButton;
+    @FXML
+    private CheckBox cardTabAsDeputyCheckBox;
+
+    //tab pan
+    @FXML
+    private TabPane tabPane;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    private void onValidateButtonClick(ActionEvent event) {
+        cardTabCustomerIDText.setText(customerIDTextField.getText());
+        cardTabBoxNoText.setText(boxNoTextField.getText());
+        cardTabAsDeputyCheckBox.setSelected(formAsDeputyCheckBox.isSelected());
+        cardTabVisitDateText.setText(getTodayDate());
+        cardTabVisitTimeText.setText(getCurrentTime());
+        moveToNextTab();
+    }
+
+    @FXML
+    private void onGrantButtonClick(ActionEvent event) {
+        moveToNextTab();
+
+    }
+
+    private void moveToNextTab() {
+        int nextTabIndex = tabPane.getSelectionModel().getSelectedIndex() + 1;
+        if (nextTabIndex < tabPane.getTabs().size()) {
+            tabPane.getSelectionModel().select(nextTabIndex);
+        }
+    }
+
+    private String getTodayDate() {
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String current = formatter.format(date);
+        LocalDate expire = LocalDate.parse(current);
+        return String.valueOf(expire);
+
+    }
+
+    private String getCurrentTime() {
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return time.format(formatter);
+    }
+
+
 }
