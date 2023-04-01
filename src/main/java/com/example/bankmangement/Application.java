3de74782ccf8920f97322bf1d8ad893d43d2220f
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-04-02 02:09:25 +0600

    Login Page added

--- src/main/java/com/example/bankmangement/Application.java
+++ src/main/java/com/example/bankmangement/Application.java
@@ -9,9 +9,9 @@ public class Application extends javafx.application.Application {
     //    Windows.getInstance().leaseAgreementWindow().show();
 //        Windows.getInstance().infoViewWindow().show();
 //        Windows.getInstance().customerContactWindow().show();
-//        Windows.getInstance().visitationWindow().show();
-    Windows.getInstance().addAccountWindow().show();
-//        Windows.getInstance().loginWindow().show();
+   Windows.getInstance().visitationWindow().show();
+//    Windows.getInstance().addAccountWindow().show();
+//    Windows.getInstance().loginWindow().show();
 
     }
 
