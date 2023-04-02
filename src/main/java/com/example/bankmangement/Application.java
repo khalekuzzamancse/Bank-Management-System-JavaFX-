ceca6a2cf405da351718ee21bdfb502c62be7add
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-04-02 19:09:45 +0600

    Login Page added

--- src/main/java/com/example/bankmangement/Application.java
+++ src/main/java/com/example/bankmangement/Application.java
@@ -6,13 +6,13 @@ import javafx.stage.Stage;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws Exception {
-     //  Windows.getInstance().leaseAgreementWindow().show();
-       Windows.getInstance().infoViewWindow().show();
-        //  Windows.getInstance().customerContactWindow().show();
-        //  Windows.getInstance().visitationWindow().show();
+        //  Windows.getInstance().leaseAgreementWindow().show();
+       // Windows.getInstance().infoViewWindow().show();
+        Windows.getInstance().customerContactWindow().show();
+         //Windows.getInstance().visitationWindow().show();
 //Windows.getInstance().addAccountWindow().show();
 //    Windows.getInstance().loginWindow().show();
-     //   Windows.getInstance().getExperimental().show();
+        //   Windows.getInstance().getExperimental().show();
 
     }
 
